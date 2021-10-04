15eb05e1c35b1e2720d9f783d635c276756a1419
jackosan <51609573+jackosan@users.noreply.github.com>
2021-10-04 07:05:41 +0200

    I01_tutorial_5-6
    Completed the tutorial videos 5 and 6 of the series

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask jumpableGround;
+
     private Animator anim;
     private SpriteRenderer sprite;
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
-    // Start is called before the first frame update
+    private enum MovementState
+    {
+        idle,
+        running,
+        jumping,
+        falling
+    }
+
     private void Start()
     {
         //extending the RigidBody
         rigidBody = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
 
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
         //get direction: if pressed left (-1), if right (+1)
@@ -29,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
         rigidBody.velocity = new Vector2(dirX * moveSpeed, rigidBody.velocity.y);
 
         //when we press space key down (not hold)
-        if (Input.GetButtonDown("Jump")) 
+        if (Input.GetButtonDown("Jump") && IsGrounded()) 
         {
 
             //change the velocity of object
@@ -38,31 +48,44 @@ public class PlayerMovement : MonoBehaviour
         }
 
         UpdateAnimationState();
-
-        
     }
 
     private void UpdateAnimationState()
     {
+        MovementState _state;
+        
         //running forward
         if (dirX > 0f)
         {
-            anim.SetBool("running", true);
+            _state = MovementState.running;
             sprite.flipX = false;
-
         }
         //running backward
         else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            _state = MovementState.running;
             sprite.flipX = true;
         }
         //not running
         else
         {
-            anim.SetBool("running", false);
+            _state = MovementState.idle;
         }
+
+        if (rigidBody.velocity.y > 0.1f)
+        {
+            _state = MovementState.jumping;
+        }
+        else if (rigidBody.velocity.y < -0.1f)
+        {
+            _state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)_state);
     }
 
-
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
 }
